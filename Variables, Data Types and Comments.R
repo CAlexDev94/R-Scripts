f135b4aab140bb36fc9149c6fd060de029da4998
CarlosAlex <deleongutierrez7@gmail.com>
2022-08-04 21:33:07 -0600

    Primeros pasos con R

---
+++ Variables, Data Types and Comments.R
@@ -0,0 +1,25 @@
+
+'----------------------------------------'
+#Adding Comments to the Code
+'----------------------------------------'
+#Create variable A with value 1
+
+A <- 1
+'--------------------------------------------'
+  #Variables <- or = assign value to variable
+'--------------------------------------------'
+#Variable String using <- 
+A <- "Hello World"
+#Variable String using =
+A = "Hello World"
+'--------------------------------------------'
+#Data Types
+'--------------------------------------------'
+A <- TRUE # Logical Data Type
+B <- "ABC" # Character Data Type
+C <- 12.3  # Numeric Data Type
+
+D <- c(1,2,3,4,5,6) # Vector 
+E <- list(1, 2, 3, "a", "b", "c") # List 
+'--------------------------------------------'
+
